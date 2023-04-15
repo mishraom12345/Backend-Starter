3155c03b6132c60370768acb123e95795b37cc5e
mishraom12345 <mishrahariom525@gmail.com>
2023-04-16 00:46:42 +0530

    writing the response based on the url

--- app.js
+++ app.js
@@ -1,9 +1,20 @@
 const http = require('http');
 
 const server = http.createServer((req, res) => {
-  console.log(req);
-  console.log("hariom mishra")
-  res.end()
+  console.log(req.method);
+  res.setHeader('Content-Type', 'text/html');
+  res.write('<html>');
+  if (req.url == '/home') {
+    res.write('<body><h2>Welcome to the Home Page</h2></body>');
+  } else if (req.url == '/about') {
+    res.write('<body><h1>Welcome to the About Page</h1></body>');
+  } else {
+    res.write('<body><h1>Welcome to the Node.js Page</h1></body>');
+  }
+  res.write('<head><title>My First Page</title></head>');
+  res.write('<body><h1>Hello from Node.js Server</h1></body>');
+  res.write('</html>');
+  res.end();
 });
 
-server.listen(4000);
+server.listen(9000); // Using port 3000
